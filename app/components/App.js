78d53fd9fa1c7abc0cb17fb80204ff80a6ef0625
Patrick Miner <patrickminer318@gmail.com>
2018-02-19 15:00:34 -0800

    Set up dev environment

---
+++ app/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>Your React Node app is set up</div>
+    )
+  }
+}
+
+export default App;
---
+++ app/server/app.js
@@ -0,0 +1,15 @@
+import path from 'path';
+import express from 'express';
+
+const app = express();
+
+const publicPath = express.static(path.join(__dirname, '../')); // sets the publicPath equal to the build folder (we start in server folder and ../ out to build folder)
+const indexPath = path.join(__dirname, '../index.html'); // sets indexPath equal to the index.html file in the build folder
+
+app.use(publicPath);
+
+app.get('/', (req, res) => {
+  res.sendFile(indexPath);
+})
+
+export default app;
