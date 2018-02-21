cf1a5c11d88f4efd35091d170d0fd7b4845f9533
Patrick Miner <patrickminer318@gmail.com>
2018-02-20 21:49:49 -0800

    Added default shape to each piece of application state

--- app/reducers/reducer_lists.js
+++ app/reducers/reducer_lists.js
@@ -1,6 +1,7 @@
 // this reducer will control board state and will have mulitple cases to catch different types of actions
 
-export default function (state = [], action) {
+export default function (state = { byId: {}, allIds: [] }, action) {
+  console.log('Inside Lists Reducer')
   switch (action.type) {
     case 'EXAMPLE CASE':
       return [...state, action.payload];
