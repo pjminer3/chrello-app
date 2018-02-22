baea3ce65445524fdd5ce542630738992f29e48e
Patrick Miner <patrickminer318@gmail.com>
2018-02-21 16:21:47 -0800

    Added proper Bootstrap navigation bar. Added brand name and Search bar

--- app/containers/header_bar.js
+++ app/containers/header_bar.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function () {
   return (
-    <div>
-      Hello, The header is here
-    </div>
+    <nav className="navbar navbar-default">
+      <div className="container-fluid">
+        {/* The brand name Chrello re-direct to homepage */}
+        <div className="navbar-header">
+          <Link to="/" className="navbar-brand">Chrello</Link>
+        </div>
+
+        {/* Searchbar */}
+        <form className="navbar-form navbar-right">
+          <div className="form-group">
+            <input type="text" className="form-control" placeholder="Search Projects..." />
+            {/* TODO: Eventually have auto-rendering dropdown list of Projects with what's typed... onChange */}
+          </div>
+        </form>
+
+      </div>
+    </nav>
   );
 }
 
