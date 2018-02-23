924f514e6a382b68f6fc2be468455ec6ec834cb1
Patrick Miner <patrickminer318@gmail.com>
2018-02-22 18:57:38 -0800

    Added 'createNewBoard' action creator and exported it

--- app/actions/index.js
+++ app/actions/index.js
@@ -2,6 +2,7 @@
 // ------------------- BEGINNING OF ACTION CREATORS
 
 const NEW_CATEGORY = 'NEW_CATEGORY';
+const NEW_BOARD = 'NEW_BOARD';
 
 const createNewCategory = (categoryName) => {
   console.log('This is New Category name: ', categoryName);
@@ -11,10 +12,20 @@ const createNewCategory = (categoryName) => {
   };
 };
 
+const createNewBoard = (boardName) => {
+  console.log('This is New Board Name: ', boardName);
+  return {
+    type: NEW_BOARD,
+    payload: boardName,
+  };
+};
+
 
 // ------------------- END OF ACTION CREATORS
 
 module.exports = {
   createNewCategory,
   NEW_CATEGORY,
+  createNewBoard,
+  NEW_BOARD,
 };
