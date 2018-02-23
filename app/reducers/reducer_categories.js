f1cee35e160ba16475c1e65df49cadd2310bca28
Patrick Miner <patrickminer318@gmail.com>
2018-02-22 22:15:36 -0800

    Added dynamically rendered default category 'Personal Boards'

--- app/reducers/reducer_categories.js
+++ app/reducers/reducer_categories.js
@@ -1,7 +1,7 @@
 // this reducer will control board state and will have mulitple cases to catch different types of actions
 import { NEW_CATEGORY } from '../actions';
 
-export default function (state = { byId: {}, allIds: [] }, action) {
+export default function (state = { byId: { id: 'Personal Boards', boards: [] }, allIds: ['Personal Boards'] }, action) {
   const { payload } = action;
 
   switch (action.type) {
@@ -17,7 +17,7 @@ export default function (state = { byId: {}, allIds: [] }, action) {
         byId: Object.assign({}, state.byId, { payload: { id: payload, boards: [] } }),
         allIds: [...state.allIds, action.payload],
       };
-      
+
     default:
       return state;
   }
