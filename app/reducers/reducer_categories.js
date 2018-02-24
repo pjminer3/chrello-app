56db9b93988becbcea7eeec21090157791bd32ae
Patrick Miner <patrickminer318@gmail.com>
2018-02-23 17:12:21 -0800

    added NEW_BOARD case to reducer_categories

--- app/reducers/reducer_categories.js
+++ app/reducers/reducer_categories.js
@@ -1,5 +1,5 @@
 // this reducer will control board state and will have mulitple cases to catch different types of actions
-import { NEW_CATEGORY } from '../actions';
+import { NEW_CATEGORY, NEW_BOARD } from '../actions';
 
 // initiate application state with 'Personal Boards' category and no boards
 export default function (state = { byId: { 'Personal Boards': { id: 'Personal Boards', boards: [] } }, allIds: ['Personal Boards'] }, action) {
@@ -18,6 +18,12 @@ export default function (state = { byId: { 'Personal Boards': { id: 'Personal Bo
         byId: Object.assign({}, state.byId, { [payload]: { id: payload, boards: [] } }),
         allIds: [...state.allIds, action.payload],
       };
+    case NEW_BOARD:
+      // payload = {boardName, categoryName}
+
+      state.byId[payload.categoryName].boards.push(payload.boardName);
+
+      return Object.assign({}, state);
 
     default:
       return state;
