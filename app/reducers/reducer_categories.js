09d51db1968b128e4d1a2f77bd8477e097715aa7
Patrick Miner <patrickminer318@gmail.com>
2018-02-23 16:53:21 -0800

    Fixed bug in reducer_categories.js. Now updates application state with new boards properly

--- app/reducers/reducer_categories.js
+++ app/reducers/reducer_categories.js
@@ -1,7 +1,8 @@
 // this reducer will control board state and will have mulitple cases to catch different types of actions
 import { NEW_CATEGORY } from '../actions';
 
-export default function (state = { byId: { id: 'Personal Boards', boards: [] }, allIds: ['Personal Boards'] }, action) {
+// initiate application state with 'Personal Boards' category and no boards
+export default function (state = { byId: { 'Personal Boards': { id: 'Personal Boards', boards: [] } }, allIds: ['Personal Boards'] }, action) {
   const { payload } = action;
 
   switch (action.type) {
@@ -14,7 +15,7 @@ export default function (state = { byId: { id: 'Personal Boards', boards: [] }, 
 
       // add new Category to total categories
       return {
-        byId: Object.assign({}, state.byId, { payload: { id: payload, boards: [] } }),
+        byId: Object.assign({}, state.byId, { [payload]: { id: payload, boards: [] } }),
         allIds: [...state.allIds, action.payload],
       };
 
