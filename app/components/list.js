f9013615f32e43756b1a131bfad51656ff12619a
Patrick Miner <patrickminer318@gmail.com>
2018-03-24 00:32:50 -0700

    List's new card form is a controlled component and changes/submits are being handled appropriately

--- app/components/list.js
+++ app/components/list.js
@@ -6,11 +6,28 @@ class List extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      cardValue: '',
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  createCard(event) {
-    return event;
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log('We create new card: ', this.state.cardValue);
+    // call new card action creator
+    this.setState({ cardValue: '' });
+
+    // add the card to the beginning of the proper list's cards array of List redux store
+    // create the Card in redux store
+      // add the card byId
+      // add the card to the allIds array
+  }
+
+  handleChange(event) {
+    this.setState({ cardValue: event.target.value });
   }
 
   render() {
@@ -22,8 +39,8 @@ class List extends Component {
           {this.props.list.cards.map(card => <Card cardId={card} key={card} />)}
 
           {/* new card */}
-          <form>
-            <input type="text" placeholder="Add new card" />
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" placeholder="Add new card" value={this.state.cardValue} onChange={this.handleChange} />
           </form>
         </div>
       </div>
