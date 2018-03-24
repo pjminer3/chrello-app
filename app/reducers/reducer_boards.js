79ea80920735a7692595d61fa5a4f2949968a731
Patrick Miner <patrickminer318@gmail.com>
2018-03-23 23:40:09 -0700

    Updated boards reducer to account for new list creation. New lists render properly

--- app/reducers/reducer_boards.js
+++ app/reducers/reducer_boards.js
@@ -1,13 +1,15 @@
 // this reducer will control board state and will have mulitple cases to catch different types of actions
 
-import { NEW_BOARD } from '../actions';
+import { NEW_BOARD, NEW_LIST } from '../actions';
 
 export default function (state = { byId: {}, allIds: [] }, action) {
   const { payload } = action;
+  let newState;
+  let lists;
 
   switch (action.type) {
     case NEW_BOARD:
-      // PAYLOAD = { boadName, categoryName }
+      // payload = { boadName, categoryName }
 
       // edge case if its a duplicate board name
       if (state.byId[payload.boardName]) {
@@ -16,9 +18,33 @@ export default function (state = { byId: {}, allIds: [] }, action) {
 
       // add board to boards application state
       return {
-        byId: Object.assign({}, state.byId, { [payload.boardName]: { id: payload.boardName, category: payload.categoryName, lists: [] } }),
+        byId: Object.assign({}, state.byId, {
+          [payload.boardName]: {
+            id: payload.boardName,
+            category: payload.categoryName,
+            lists: [],
+          },
+        }),
         allIds: [payload.boardName, ...state.allIds],
       };
+
+    case NEW_LIST:
+      // payload = { listName, boardName }
+
+      // edge case: it's a duplicate listName
+      if (state.byId[payload.boardName].lists.indexOf(payload.listName) >= 0) {
+        return state;
+      }
+
+      // define new state and lists
+      newState = JSON.parse(JSON.stringify(state));
+      lists = newState.byId[payload.boardName].lists;
+
+      // add list to proper board
+      newState.byId[payload.boardName].lists = [payload.listName, ...lists];
+
+      return newState;
+
     default:
       return state;
   }
