a35ef4b9f540157ac480aa13715c7e867d02736e
Patrick Miner <patrickminer318@gmail.com>
2018-03-31 01:30:18 -0700

    Added boilerplate for authentication

---
+++ app/server/middleware/auth.js
@@ -0,0 +1,10 @@
+// Authentication middleware
+
+import Promise from 'bluebird';
+import models from '../models';
+
+const createSession = (request, response, next) => {
+
+};
+
+export { createSession };
