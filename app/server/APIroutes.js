07d6295e93eeeece5183644bfec3a075acb61b42
Patrick Miner <patrickminer318@gmail.com>
2018-04-01 15:22:38 -0700

    Added logging/parsing middleware. Added API router for routes interacting with the database

--- app/server/APIroutes.js
+++ app/server/APIroutes.js
@@ -3,15 +3,15 @@ const router = require('express').Router();
 
 // connect controller methods to their corresponding routes
 router.get('/category', controller.category.get);
-router.get('/category', controller.category.post);
+router.post('/category', controller.category.post);
 
 router.get('/board', controller.category.get);
-router.get('/board', controller.category.post);
+router.post('/board', controller.category.post);
 
 router.get('/list', controller.category.get);
-router.get('/list', controller.category.post);
+router.post('/list', controller.category.post);
 
 router.get('/card', controller.category.get);
-router.get('/card', controller.category.post);
+router.post('/card', controller.category.post);
 
 export default router;
--- app/server/app.js
+++ app/server/app.js
@@ -4,17 +4,30 @@ import bodyParser from 'body-parser';
 import morgan from 'morgan';
 
 import db from './database/index';
-import router from './routes';
+import router from './APIroutes';
 
 const app = express();
+
+// logging and parsing
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+// set up our API routes
+app.use('/api', router);
 
 const publicPath = express.static(path.join(__dirname, '../')); // sets the publicPath equal to the build folder
 const indexPath = path.join(__dirname, '../index.html'); // sets indexPath equal to the index.html file in the build folder
 
+// DANNY! WHAT DOES THIS DO!
 app.use(publicPath);
 
-app.get("/home", (req, res) => {
+app.get("/", (request, response) => {
   res.sendFile(indexPath);
+});
+
+app.get("/:category/:board", (request, response) => {
+  console.log(request.params);
+  response.send('You\'re learning!');
 });
 
 export default app;
