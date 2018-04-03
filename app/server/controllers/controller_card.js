7dc2d846ff84d37bb87752923b6f8ba7eedb97e9
Patrick Miner <patrickminer318@gmail.com>
2018-04-02 23:15:30 -0700

    Created GET route for retreiving cards from a given list

--- app/server/controllers/controller_card.js
+++ app/server/controllers/controller_card.js
@@ -3,6 +3,18 @@ import { Card } from '../database';
 const card = {
     get: function(request, response) {
         console.log('card.get was called');
+        const { listId } = request.params;
+
+        Card.findAll({
+            where: { listId },
+            attributes: ['cardContent', 'id']
+        })
+          .then(cards => {
+              response.json(cards);
+          })
+          .catch(err => {
+              console.log('There was an error in retreiving cards: ', err);
+          });
     }, 
     post: function(request, response) {
         console.log('card.post was called');
